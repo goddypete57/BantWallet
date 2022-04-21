2064224ebb6268c53224959a08292f5dcce3a987
ANDY <andymaking"gmail.com>
2022-04-21 12:22:02 +0100

    home

--- app/src/main/java/com/example/bantcoin/Home.kt
+++ app/src/main/java/com/example/bantcoin/Home.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,8 +24,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
 
 
+@OptIn(ExperimentalPagerApi::class)
 @Preview
 @Composable
 fun HomeScreen() {
@@ -73,10 +73,11 @@ fun HomeScreen() {
             }
         },
         content = {
-            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top){
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally){
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp), horizontalArrangement = Arrangement.Center) {
                     androidx.compose.material.Text(text = "9.3729 ETH", fontSize = 55.sp, fontFamily = sanSansFamily, fontWeight = FontWeight.SemiBold, modifier = Modifier
-                        .padding(top = 24.dp)
                         .graphicsLayer(alpha = .99f)
                         .drawWithCache {
                             onDrawWithContent {
@@ -86,10 +87,74 @@ fun HomeScreen() {
                         }
                     )
                 }
-                Row() {
-                    androidx.compose.material.Text(text = "$19,328.19")
-                    androidx.compose.material.Text(text = "+0.7%")
+                Row(modifier = Modifier.padding(top = 8.dp)) {
+                    Text(text = "$19,328.19", fontFamily = sanSansFamily, fontWeight = FontWeight.SemiBold, fontSize = 21.sp)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = "+0.7%", fontFamily = sanSansFamily, fontWeight = FontWeight.SemiBold, fontSize = 21.sp, color = Color(35, 167, 87))
                 }
+                Row(modifier = Modifier.padding(top = 8.dp)) {
+                    Row(
+                        Modifier
+                            .clip(RoundedCornerShape(50))
+                            .background(color = Color(244, 246, 249))
+                            .padding(horizontal = 15.dp, vertical = 8.dp)) {
+                        Text(text = "0x7aCaaba8238.....96eEf19", fontFamily = sanSansFamily, fontWeight = FontWeight.SemiBold, fontSize = 21.sp)
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Icon(painter = painterResource(id = R.drawable.copy), contentDescription = "icon", modifier = Modifier
+                            .height(18.dp)
+                            .width(16.dp)
+                            .clickable { /*TODO*/ })
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp), horizontalArrangement = Arrangement.Center) {
+                    Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(50), elevation = ButtonDefaults.elevation(0.dp), colors = ButtonDefaults.buttonColors(Color.Transparent), modifier = Modifier
+                        .height(43.dp)
+                        .clip(
+                            RoundedCornerShape(50)
+                        )
+                        .background(colorGrade)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Icon(painter = painterResource(id = R.drawable.send), tint = Color.White, contentDescription = "send", modifier = Modifier
+                            .height(16.dp)
+                            .width(17.dp))
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = "Send", fontFamily = sanSansFamily, color = Color.White, fontWeight = FontWeight.SemiBold, fontSize = 19.sp)
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(50), elevation = ButtonDefaults.elevation(0.dp), colors = ButtonDefaults.buttonColors(Color.Transparent), modifier = Modifier
+                        .height(43.dp)
+                        .clip(
+                            RoundedCornerShape(50)
+                        )
+                        .background(colorGrade)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Icon(painter = painterResource(id = R.drawable.recieve), tint = Color.White, contentDescription = "Receive", modifier = Modifier
+                            .height(16.dp)
+                            .width(17.dp))
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = "Receive", color = Color.White, fontFamily = sanSansFamily, fontWeight = FontWeight.SemiBold, fontSize = 19.sp)
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(50), elevation = ButtonDefaults.elevation(0.dp), colors = ButtonDefaults.buttonColors(Color.Transparent), modifier = Modifier
+                        .height(43.dp)
+                        .clip(
+                            RoundedCornerShape(50)
+                        )
+                        .background(colorGrade)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Icon(painter = painterResource(id = R.drawable.buy), tint = Color.White, contentDescription = "Buy", modifier = Modifier
+                            .height(16.dp)
+                            .width(17.dp))
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = "Buy", fontFamily = sanSansFamily, color = Color.White, fontWeight = FontWeight.SemiBold, fontSize = 19.sp)
+                    }
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                TabScreen()
+
             }
 
         }
